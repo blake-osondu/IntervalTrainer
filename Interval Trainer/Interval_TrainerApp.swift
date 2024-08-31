7a93dd4ce2399e436634afdc09fc240a1a762907
Blake <45019373+kinnectusAdmin@users.noreply.github.com>
2024-08-30 23:40:24 -0400

    Initial Commit

---
+++ Interval Trainer/Interval_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interval_TrainerApp.swift
+//  Interval Trainer
+//
+//  Created by Blake Osonduagwueki on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Interval_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
